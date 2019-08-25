69acacfe45eb498725211b5407c68ce8190c3e6e
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2019-08-25 16:14:48 -0700

    feat(types): Create types/enums.ts
    Create the file that will hold all of the ENUMs used by the SFDX-Falcon
    Library.

---
+++ packages/types/src/enums.ts
@@ -0,0 +1,47 @@
+//─────────────────────────────────────────────────────────────────────────────────────────────────┐
+/**
+ * @file          packages/types/core.ts
+ * @copyright     Vivek M. Chawla / Salesforce - 2019
+ * @author        Vivek M. Chawla <@VivekMChawla>
+ * @summary       Collection of ENUMs used by the SFDX-Falcon Library.
+ * @description   Collection of ENUMs used by the SFDX-Falcon Library.
+ * @license       MIT
+ */
+//─────────────────────────────────────────────────────────────────────────────────────────────────┘
+if (true) {}
+
+/**
+ * Enum. Represents the various CLI log level flag values.
+ */
+export enum SfdxCliLogLevel {
+  TRACE = 'trace',
+  DEBUG = 'debug',
+  INFO  = 'info',
+  WARN  = 'warn',
+  ERROR = 'error',
+  FATAL = 'fatal'
+}
+
+/**
+ * Enum. Represents a generic set of commonly used Status values.
+ */
+export enum Status {
+  NOT_STARTED = 'NOT_STARTED',
+  WAITING     = 'WAITING',
+  WORKING     = 'WORKING',
+  COMPLETE    = 'COMPLETE',
+  PENDING     = 'PENDING',
+  SKIPPED     = 'SKIPPED',
+  FAILED      = 'FAILED'
+}
+
+/**
+ * Enum. Represents the various types/states of a Status Message.
+ */
+export enum StatusMessageType {
+  ERROR   = 'error',
+  INFO    = 'info',
+  SUCCESS = 'success',
+  WARNING = 'warning',
+  FATAL   = 'fatal'
+}
