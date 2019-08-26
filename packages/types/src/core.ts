8110c7806354a7c6aad58313aba7a6c9d84abbc6
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2019-08-25 21:06:55 -0700

    feat(types): Add core and SObject types

--- packages/types/src/core.ts
+++ packages/types/src/core.ts
@@ -9,24 +9,45 @@
  */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┘
 // Import External Modules/Types
-//import {Connection}           from  '@salesforce/core';
-//import {AnyJson}              from  '@salesforce/ts-types';
-import {JsonMap}              from  '@salesforce/ts-types';
-//import {Observable}           from  'rxjs';
-//import {Observer}             from  'rxjs';
-//import {Subscriber}           from  'rxjs';
-//import {Questions}            from  'yeoman-generator';
-//import {Question}             from  'yeoman-generator';
+import  {JsonMap}             from  '@salesforce/ts-types';
+
+// Import Internal Modules/Types
+import  {StatusMessageType}   from  './enum';
 
 /**
  * @module types
  */
 
-
 /**
  * Type. Represents the constructor for a Class, ie. something that can be the right operand of the instanceof operator.
  */
 export type ClassConstructor = any;  // tslint:disable-line: no-any
+
+/**
+ * Interface. Represents options that determine how a generic interval operates.
+ */
+export interface IntervalOptions extends JsonMap {
+  /** The initial interval, in seconds. */
+  initial?:     number;
+  /** The amount to increment the interval by, in seconds, each time it completes. */
+  incrementBy?: number;
+  /** The maximum value, in seconds, that the interval can grow to. */
+  maximum?:     number;
+  /** The number of seconds before the interval-based operation times-out */
+  timeout?:     number;
+}
+
+/**
+ * Interface. Represents a "state aware" message. Contains a title, a message, and a type.
+ */
+export interface StatusMessage extends JsonMap {
+  /** Required. The title of the status message. */
+  title:    string;
+  /** Required. The text of the status message. */
+  message:  string;
+  /** Required. The type of the status message. */
+  type:     StatusMessageType;
+}
 
 /**
  * Interface. Allows for specification of a message string and chalk-specific styling information.
---
+++ packages/types/src/sobject.ts
@@ -0,0 +1,49 @@
+//─────────────────────────────────────────────────────────────────────────────────────────────────┐
+/**
+ * @file          packages/types/sobject.ts
+ * @copyright     Vivek M. Chawla / Salesforce - 2019
+ * @author        Vivek M. Chawla <@VivekMChawla>
+ * @summary       All types/interfaces that could not be organized into one of the other buckets.
+ * @description   All types/interfaces that could not be organized into one of the other buckets.
+ * @license       MIT
+ */
+//─────────────────────────────────────────────────────────────────────────────────────────────────┘
+if (true) {} // NoOp statement. Stops JSDocs from attaching file header comments to the first export, below.
+
+/**
+ * Type. Represents an SObject Record ID.
+ */
+export type SObjectRecordId = string;
+
+/**
+ * Interface. Represents a baseline SObject.
+ */
+export interface SObject {
+  id?:    string;
+  name?:  string;
+}
+
+/**
+ * Interface. Represents the Salesforce Profile SObject.
+ */
+export type Profile = SObject;
+
+/**
+ * Interface. Represents the Salesforce PermissionSetAssignment SObject.
+ */
+export interface PermissionSetAssignment extends SObject {
+  PermissionSetId:  string;
+  AssigneeId:       string;
+}
+
+/**
+ * Interface. Represents the Salesforce User SObject.
+ */
+export interface User extends SObject {
+  username?: string;
+}
+
+/**
+ * Type. Alias for an array of objects that may have "Id" and "Name" properties.
+ */
+export type SObjectFindResult = Array<{Id?: string; Name?: string; }>;
