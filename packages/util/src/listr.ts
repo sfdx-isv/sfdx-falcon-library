067f549d0db087e150d13d435b35c47304d6cee5
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2019-09-10 13:11:26 -0700

    refactor(util): Change ret type on chooseListrRenderer()
    Update return type on chooseListrRenderer() so Listr object creation
    doesn't reject return vals from this function.

--- packages/util/src/listr.ts
+++ packages/util/src/listr.ts
@@ -10,32 +10,32 @@
  * @license       MIT
  */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┘
-// Import SFDX-Falcon Classes & Functions
-import {SfdxFalconDebug}  from  '@sfdx-falcon/debug';   // Class. Specialized debug provider for SFDX-Falcon code.
+// Import External Libraries, Modules, and Types
+import  {ListrRendererValue}  from  'listr';                // Type. Defines the possible values for a Listr Renderer.
 
-// Import SFDX-Falcon Types
-import {AnyConstructor}   from  '@sfdx-falcon/types';   // Interface. Represents the request body required to create a Bulk API 2.0 job.
+// Import SFDX-Falcon Classes & Functions
+import  {SfdxFalconDebug}     from  '@sfdx-falcon/debug';   // Class. Specialized debug provider for SFDX-Falcon code.
 
 // Requires
-const falconUpdateRenderer  = require('falcon-listr-update-renderer');  // Custom renderer for Listr
+const falconUpdateRenderer  = require('falcon-listr-update-renderer');  // Custom SFDX-Falcon renderer for Listr.
 
 // Set the File Local Debug Namespace
-const dbgNs = 'UTILITY:listr:';
+const dbgNs = '@sfdx-falcon:util:listr:';
 SfdxFalconDebug.msg(`${dbgNs}`, `Debugging initialized for ${dbgNs}`);
 
 
 // ────────────────────────────────────────────────────────────────────────────────────────────────┐
 /**
  * @function    chooseListrRenderer
- * @returns     {ClassConstructor|string} Constructor for a custom Listr Renderer or a string
- *              designating one of the default renderers.
+ * @returns     {ListrRendererValue<unknown>} Constructor for a custom Listr Renderer or a string
+ *              string designating one of the default renderers.
  * @description Returns either a custom Listr Renderer or a string designating the default "verbose"
  *              renderer depending on whether or not the user specified any Debug Namespaces when
  *              the currently running command was initiated.
  * @public
  */
 // ────────────────────────────────────────────────────────────────────────────────────────────────┘
-export function chooseListrRenderer():AnyConstructor|string {
+export function chooseListrRenderer():ListrRendererValue<unknown> {
   if (SfdxFalconDebug.enabledDebugNamespaceCount === 0) {
     return falconUpdateRenderer;
   }
