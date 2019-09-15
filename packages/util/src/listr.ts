beffeb6d8ed3c65718ba1551c4e86d320b2727a8
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2019-09-15 06:34:16 -0700

    feat(util): Allow preference of Listr renderer
    Update chooseListrRenderer() so it allows the caller to indicate a
    preference for a specific renderer. Mostly used to prefer the SILENT
    renderer.

--- packages/util/src/listr.ts
+++ packages/util/src/listr.ts
@@ -11,22 +11,28 @@
  */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┘
 // Import External Libraries, Modules, and Types
-import  {ListrRendererValue}  from  'listr';                // Type. Defines the possible values for a Listr Renderer.
+import  FalconUpdateRenderer  = require('falcon-listr-update-renderer');  // Custom SFDX-Falcon renderer for Listr.
+import  {ListrRendererValue}    from    'listr';                          // Type. Defines the possible values for a Listr Renderer.
+import  SilentRenderer        = require('listr-silent-renderer');         // Specialized Listr Renderer that suppresses all output, other than errors.
+
+// Import SFDX-Falcon Libraries
+import  {TypeValidator}         from  '@sfdx-falcon/validator'; // Library. Helper functions related to Type Validation.
 
 // Import SFDX-Falcon Classes & Functions
-import  {SfdxFalconDebug}     from  '@sfdx-falcon/debug';   // Class. Specialized debug provider for SFDX-Falcon code.
-
-// Requires
-const falconUpdateRenderer  = require('falcon-listr-update-renderer');  // Custom SFDX-Falcon renderer for Listr.
+import  {SfdxFalconDebug}       from  '@sfdx-falcon/debug';     // Class. Specialized debug provider for SFDX-Falcon code.
 
 // Set the File Local Debug Namespace
-const dbgNs = '@sfdx-falcon:util:listr:';
-SfdxFalconDebug.msg(`${dbgNs}`, `Debugging initialized for ${dbgNs}`);
+const dbgNs = '@sfdx-falcon:util:listr';
+SfdxFalconDebug.msg(`${dbgNs}:`, `Debugging initialized for ${dbgNs}`);
 
 
 // ────────────────────────────────────────────────────────────────────────────────────────────────┐
 /**
  * @function    chooseListrRenderer
+ * @param       {string}  [preference=''] Optional. Indicates the caller's preference for a specific
+ *              renderer. Valid choices are `custom`, `verbose`, and `silent`. Usually this will be
+ *              used to force the `verbose` renderer during dev/test or to force the `silent`
+ *              renderer for unattended workloads.
  * @returns     {ListrRendererValue<unknown>} Constructor for a custom Listr Renderer or a string
  *              string designating one of the default renderers.
  * @description Returns either a custom Listr Renderer or a string designating the default "verbose"
@@ -35,11 +41,34 @@ SfdxFalconDebug.msg(`${dbgNs}`, `Debugging initialized for ${dbgNs}`);
  * @public
  */
 // ────────────────────────────────────────────────────────────────────────────────────────────────┘
-export function chooseListrRenderer():ListrRendererValue<unknown> {
-  if (SfdxFalconDebug.enabledDebugNamespaceCount === 0) {
-    return falconUpdateRenderer;
+export function chooseListrRenderer(preference:string=''):ListrRendererValue<unknown> {
+
+  // Define function-local and external debug namespaces.
+  const funcName    = `chooseListrRenderer`;
+  const dbgNsLocal  = `${dbgNs}:${funcName}`;
+
+  // Reflect incoming arguments.
+  SfdxFalconDebug.obj(`${dbgNsLocal}:arguments:`, arguments);
+
+  // Make sure the preference variable is valid.
+  if (TypeValidator.isNullInvalidString(preference)) {
+    preference = '';
   }
-  else {
-    return 'verbose';
+
+  // Choose the right renderer.
+  switch (preference.toLowerCase()) {
+    case 'custom':
+      return FalconUpdateRenderer;
+    case 'verbose':
+      return 'verbose';
+    case 'silent':
+      return SilentRenderer;
+    default:
+      if (SfdxFalconDebug.enabledDebugNamespaceCount === 0) {
+        return FalconUpdateRenderer;
+      }
+      else {
+        return 'verbose';
+      }
   }
 }
