5d833a408862813e3af29bd1fbbd001d3695c62f
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2019-09-16 18:51:31 -0700

    refactor(task-library): Change 'result' pkg refs to 'status'

--- packages/task-library/src/sfdx.ts
+++ packages/task-library/src/sfdx.ts
@@ -35,25 +35,11 @@ const dbgNs = '@sfdx-falcon:task-library:sfdx:';
 SfdxFalconDebug.msg(`${dbgNs}`, `Debugging initialized for ${dbgNs}`);
 
 
-/**
- * Interface. Represents initialization requirements related to SFDX configuration.
- */
-export interface SfdxEnvironmentRequirements {
-  standardOrgs:     boolean;
-  scratchOrgs:      boolean;
-  devHubOrgs:       boolean;
-  envHubOrgs:       boolean;
-  managedPkgOrgs:   boolean;
-  unmanagedPkgOrgs: boolean;
-}
-
-
-
 
 
 // ────────────────────────────────────────────────────────────────────────────────────────────────┐
 /**
- * @function    sfdxInitTasks
+ * @function    ????
  * @returns     {ListrObject}  A "runnable" Listr Object
  * @description Returns a Listr-compatible Task Object that contains a number of sub-tasks which
  *              inspect the connected orgs in the local SFDX environment and build Inquirer "choice
@@ -61,61 +47,3 @@ export interface SfdxEnvironmentRequirements {
  * @public
  */
 // ────────────────────────────────────────────────────────────────────────────────────────────────┘
-/*
-export function sfdxInitTasks():ListrObject {
-
-  // Make sure the calling scope has a valid context variable.
-  validateSharedData.call(this);
-
-  // Grab Generator Requirements out of Shared Data.
-  const generatorRequirements = this.sharedData['generatorRequirements']  as GeneratorRequirements;
-  SfdxFalconDebug.obj(`${dbgNs}sfdxInitTasks:generatorRequirements:`, generatorRequirements);
-
-  // Build and return a Listr Object.
-  return new listr(
-    [
-      {
-        // PARENT_TASK: Local SFDX Configuration
-        title: 'Inspecting Local SFDX Configuration',
-        enabled:() => typeValidator.isNotNullInvalidObject(generatorRequirements)
-                      && (generatorRequirements.standardOrgs        === true
-                          || generatorRequirements.scratchOrgs      === true
-                          || generatorRequirements.devHubOrgs       === true
-                          || generatorRequirements.envHubOrgs       === true
-                          || generatorRequirements.managedPkgOrgs   === true
-                          || generatorRequirements.unmanagedPkgOrgs === true),
-        task: listrContext => {
-          return new listr(
-            [
-              scanConnectedOrgs.call(this),
-              identifyDevHubs.call(this),
-              identifyEnvHubs.call(this),
-              identifyPkgOrgs.call(this),
-              buildDevHubAliasList.call(this),
-              buildEnvHubAliasList.call(this),
-              buildPkgOrgAliasList.call(this),
-              buildStandardOrgAliasList.call(this),
-              buildScratchOrgAliasList.call(this)
-            ],
-            // SUBTASK OPTIONS: (SFDX Config Tasks)
-            {
-              concurrent: false,
-              collapse:   false,
-              renderer:   chooseListrRenderer()
-            }
-          );
-        }
-      }
-    ],
-    {
-      // PARENT_TASK OPTIONS: (Git Validation/Initialization)
-      concurrent: false,
-      collapse:   false,
-      renderer:   chooseListrRenderer()
-    }
-  );
-}
-//*/
-
-
-
