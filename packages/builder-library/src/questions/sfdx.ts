09b4b9789c4556eca63c67fe2634f43a0560eab2
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2019-10-18 07:35:23 -0700

    feat(builder-library): Add ConfirmNoTargetOrg builder

--- packages/builder-library/src/questions/sfdx.ts
+++ packages/builder-library/src/questions/sfdx.ts
@@ -4,40 +4,31 @@
  * @copyright     2019, Vivek M. Chawla / Salesforce. All rights reserved.
  * @license       BSD-3-Clause For full license text, see the LICENSE file in the repo root or
  *                `https://opensource.org/licenses/BSD-3-Clause`
- * @file          packages/builder-library/src/tasks/sfdx.ts
- * @summary       Exports a library of Task Builder functions related to SFDX.
- * @description   Exports a library of Task Builder functions related to SFDX.
+ * @file          packages/builder-library/src/questions/sfdx.ts
+ * @summary       Exports `Builder` classes for questions related to Salesforce DX functionality.
+ * @description   Exports `Builder` classes for questions related to Salesforce DX functionality.
  */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┘
 // Import External Libraries, Modules, and Types
-//import  {ListrTask}                 from  'listr';    // Interface. Represents a Task object as defined by Listr.
-//import  Listr =                     require('listr'); // Provides asynchronous list with status of task completion.
-//import * as path                    from  'path';     // Node's built-in path library.
 
 // Import SFDX-Falcon Libraries
-//import  {GitUtil}                   from  '@sfdx-falcon/util';          // Library. Git utility helper functions.
-//import  {ListrUtil}                 from  '@sfdx-falcon/util';          // Library. Listr utility helper functions.
 import  {TypeValidator}             from  '@sfdx-falcon/validator';     // Library of Type Validation helper functions.
 
 // Import SFDX-Falcon Classes & Functions
 import  {InterviewQuestionsBuilder} from  '@sfdx-falcon/builder';       // Class. Classes derived from QuestionsBuilder can be used to build an Inquirer Questions object.
-//import  {determineDbgNsExt}         from  '@sfdx-falcon/builder';       // Function. Given an ExternalContext, the name of the calling class and function, as well as an alternative debug namespace to use if the ExternalContext doesn't have the appropriate base, returns the correct External Debug Namespace string.
 import  {SfdxFalconDebug}           from  '@sfdx-falcon/debug';         // Class. Provides custom "debugging" services (ie. debug-style info to console.log()).
+import  {SfdxFalconPrompt}          from  '@sfdx-falcon/prompt';        // Class. Allows easy creation of Inquirer prompts that have a "confirmation" question that can be used to restart collection of the information.
 //import  {SfdxFalconError}           from  '@sfdx-falcon/error';         // Class. Extends SfdxError to provide specialized error structures for SFDX-Falcon modules.
-//import  {SfdxFalconTask}            from  '@sfdx-falcon/task';          // Class. Abstraction of a single Listr Task with a lot of extra functionality bundled in.
 
 // Import SFDX-Falcon Types
 import  {ExternalContext}           from  '@sfdx-falcon/builder';       // Interface. Collection of key data structures that represent the overall context of the external environment inside of which some a set of specialized logic will be run.
 import  {InquirerChoices}           from  '@sfdx-falcon/types';
+import  {JsonMap}                   from  '@sfdx-falcon/types';         // Interface. Any JSON-compatible object.
 import  {Questions}                 from  '@sfdx-falcon/types';         // Type. Alias to the Questions type from yeoman-generator. This is the "official" type for SFDX-Falcon.
-//import  {ListrContextFinalizeGit}   from  '@sfdx-falcon/types';         // Interface. Represents the Listr Context variables used by the "finalizeGit" task collection.
-//import  {ListrObject}               from  '@sfdx-falcon/types';         // Interface. Represents a "runnable" Listr object (ie. an object that has the run() method attached).
-//import  {ShellExecResult}           from  '@sfdx-falcon/types';         // Interface. Represents the result of a call to shell.execL().
-//import { waitASecond } from '@sfdx-falcon/util/lib/async';
 
 // Set the File Local Debug Namespace
-const dbgNs = '@sfdx-falcon:builder-library:questions:sfdx';
-SfdxFalconDebug.msg(`${dbgNs}:`, `Debugging initialized for ${dbgNs}`);
+const dbgNs = '@sfdx-falcon:builder-library:questions';
+SfdxFalconDebug.msg(`${dbgNs}:`, `Debugging initialized for ${dbgNs}(sfdx)`);
 
 
 //─────────────────────────────────────────────────────────────────────────────────────────────────┐
@@ -46,7 +37,6 @@ SfdxFalconDebug.msg(`${dbgNs}:`, `Debugging initialized for ${dbgNs}`);
  * @extends     InterviewQuestionsBuilder
  * @summary     Interview Questions Builder for choosing a single salesforce org.
  * @description Interview Questions Builder for choosing a single salesforce org.
- * @public
  */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┘
 export class ChooseSingleOrg extends InterviewQuestionsBuilder {
@@ -63,8 +53,7 @@ export class ChooseSingleOrg extends InterviewQuestionsBuilder {
    * @param       {ExternalContext} extCtx  Required.
    * @param       {InquirerChoices} scratchOrgChoices Required.
    * @param       {InquirerChoices} standardOrgChoices  Required.
-   * @description Constructs a Tm1Transform object.
-   * @public
+   * @param       {object}  [promptStrings] Optional.
    */
   //───────────────────────────────────────────────────────────────────────────┘
   constructor(extCtx:ExternalContext, scratchOrgChoices:InquirerChoices, standardOrgChoices:InquirerChoices,
@@ -73,25 +62,18 @@ export class ChooseSingleOrg extends InterviewQuestionsBuilder {
     // Call the superclass constructor.
     super(extCtx);
 
-    // Define local and external debug namespaces.
-    const className   = this.constructor.name;
-    const funcName    = `constructor`;
-    const dbgNsLocal  = `${dbgNs}:${className}:${funcName}`;
-    const dbgNsExt    = `${this.dbgNsExt}:${className}:${funcName}`;
-
-    // Debug incoming arguments.
-    SfdxFalconDebug.obj(`${dbgNsLocal}:arguments:`, arguments);
-    SfdxFalconDebug.obj(`${dbgNsExt}:arguments:`,   arguments);
+    // Initialize debug for this method.
+    const dbgNS = this.initializeDebug(dbgNs, `constructor`, arguments);
 
     // Validate incoming arguments.
-    TypeValidator.throwOnEmptyNullInvalidArray(standardOrgChoices,  `${dbgNsExt}`,  `standardOrgChoices`);
-    TypeValidator.throwOnEmptyNullInvalidArray(scratchOrgChoices,   `${dbgNsExt}`,  `scratchOrgChoices`);
-    TypeValidator.throwOnNullInvalidObject    (promptStrings,       `${dbgNsExt}`,  `promptStrings`);
+    TypeValidator.throwOnEmptyNullInvalidArray(standardOrgChoices,  `${dbgNS.ext}`,  `standardOrgChoices`);
+    TypeValidator.throwOnEmptyNullInvalidArray(scratchOrgChoices,   `${dbgNS.ext}`,  `scratchOrgChoices`);
+    TypeValidator.throwOnNullInvalidObject    (promptStrings,       `${dbgNS.ext}`,  `promptStrings`);
 
     // Validate optional arguments.
-    if (TypeValidator.isNotNullUndefined(promptStrings.promptIsScratchOrg))       TypeValidator.throwOnEmptyNullInvalidString(promptStrings.promptIsScratchOrg,       `${dbgNsExt}`,  `promptStrings.promptIsScratchOrg`);
-    if (TypeValidator.isNotNullUndefined(promptStrings.promptScratchOrgChoice))   TypeValidator.throwOnEmptyNullInvalidString(promptStrings.promptScratchOrgChoice,   `${dbgNsExt}`,  `promptStrings.promptScratchOrgChoice`);
-    if (TypeValidator.isNotNullUndefined(promptStrings.promptStandardOrgChoice))  TypeValidator.throwOnEmptyNullInvalidString(promptStrings.promptStandardOrgChoice,  `${dbgNsExt}`,  `promptStrings.promptStandardOrgChoice`);
+    if (TypeValidator.isNotNullUndefined(promptStrings.promptIsScratchOrg))       TypeValidator.throwOnEmptyNullInvalidString(promptStrings.promptIsScratchOrg,       `${dbgNS.ext}`,  `promptStrings.promptIsScratchOrg`);
+    if (TypeValidator.isNotNullUndefined(promptStrings.promptScratchOrgChoice))   TypeValidator.throwOnEmptyNullInvalidString(promptStrings.promptScratchOrgChoice,   `${dbgNS.ext}`,  `promptStrings.promptScratchOrgChoice`);
+    if (TypeValidator.isNotNullUndefined(promptStrings.promptStandardOrgChoice))  TypeValidator.throwOnEmptyNullInvalidString(promptStrings.promptStandardOrgChoice,  `${dbgNS.ext}`,  `promptStrings.promptStandardOrgChoice`);
     
     // Initialize member variables.
     this.scratchOrgChoices        = scratchOrgChoices;
@@ -106,7 +88,6 @@ export class ChooseSingleOrg extends InterviewQuestionsBuilder {
    * @method      build
    * @returns     {Questions}
    * @description Builds the Interview Questions.
-   * @public
    */
   //───────────────────────────────────────────────────────────────────────────┘
   public build():Questions {
@@ -131,6 +112,72 @@ export class ChooseSingleOrg extends InterviewQuestionsBuilder {
         message:  this.promptScratchOrgChoice,
         choices:  this.scratchOrgChoices,
         when:     answerHash => (answerHash.isScratchOrg === true && this.scratchOrgChoices.length > 0)
+      }
+    ];
+  }
+}
+
+//─────────────────────────────────────────────────────────────────────────────────────────────────┐
+/**
+ * @class       ConfirmNoTargetOrg
+ * @extends     InterviewQuestionsBuilder
+ * @summary     Interview Questions Builder for confirming refusal of a Target Org selection.
+ * @description Interview Questions Builder for confirming refusal of a Target Org selection.
+ */
+//─────────────────────────────────────────────────────────────────────────────────────────────────┘
+export class ConfirmNoTargetOrg extends InterviewQuestionsBuilder {
+
+  public promptStartOver: string;
+
+  //───────────────────────────────────────────────────────────────────────────┐
+  /**
+   * @constructs  ConfirmNoTargetOrg
+   * @param       {ExternalContext} extCtx  Required.
+   * @param       {object}  [promptStrings] Optional.
+   */
+  //───────────────────────────────────────────────────────────────────────────┘
+  constructor(extCtx:ExternalContext, promptStrings:{promptStartOver?: string}={}) {
+
+    // Call the superclass constructor.
+    super(extCtx);
+
+    // Initialize debug for this method.
+    const dbgNS = this.initializeDebug(dbgNs, `constructor`, arguments);
+
+    // Validate incoming arguments.
+    TypeValidator.throwOnNullInvalidObject(promptStrings, `${dbgNS.ext}`, `promptStrings`);
+
+    // Validate optional arguments.
+    if (TypeValidator.isNotNullUndefined(promptStrings.promptStartOver))  TypeValidator.throwOnEmptyNullInvalidString(promptStrings.promptStartOver,  `${dbgNS.ext}`, `promptStrings.promptStartOver`);
+    
+    // Initialize member variables.
+    this.promptStartOver = promptStrings.promptStartOver || `Selecting a target org is required. Would you like to see the choices again?`;
+  }
+
+  //───────────────────────────────────────────────────────────────────────────┐
+  /**
+   * @method      build
+   * @returns     {Questions}
+   * @description Builds the Interview Questions.
+   */
+  //───────────────────────────────────────────────────────────────────────────┘
+  public build(buildCtx:SfdxFalconPrompt<JsonMap>):Questions {
+
+    // Initialize debug for this method.
+    const dbgNS = this.initializeDebug(dbgNs, `build`, arguments);
+
+    // Validate the Build Context.
+    TypeValidator.throwOnInvalidInstance        (buildCtx, SfdxFalconPrompt,              `${dbgNS.ext}`, `BuildContext`);
+    TypeValidator.throwOnEmptyNullInvalidString (buildCtx.userAnswers.targetOrgUsernameXX,  `${dbgNS.ext}`, `BuildContext.userAnswers.targetOrgUsername`);
+
+    // Build and return the Questions object.
+    return [
+      {
+        type:     'confirm',
+        name:     'restart',
+        message:  this.promptStartOver,
+        default:  true,
+        when:     buildCtx.userAnswers.targetOrgUsername === 'NOT_SPECIFIED'
       }
     ];
   }
