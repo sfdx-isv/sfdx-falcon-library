9e4b897d4e134ec7982291f83fca0ca89fafdbe0
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2019-10-17 18:13:27 -0700

    refactor(types): Change entrypoint to index.ts

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -4,7 +4,7 @@
  * @copyright     2019, Vivek M. Chawla / Salesforce. All rights reserved.
  * @license       BSD-3-Clause For full license text, see the LICENSE file in the repo root or
  *                `https://opensource.org/licenses/BSD-3-Clause`
- * @file          packages/types/types.ts
+ * @file          packages/types/index.ts
  * @summary       Common collection of useful Salesforce-related types.
  * @description   Common collection of useful Salesforce-related types. Used by modules in the
  *                SFDX-Falcon Library but may also be useful any developer using TypeScript to
