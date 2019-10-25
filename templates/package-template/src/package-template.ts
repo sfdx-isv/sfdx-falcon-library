27f3aa878bbd0736e21e8551b4311cda0e82e8ed
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2019-10-24 21:08:49 -0700

    chore(template): Update file comment header w/correct format

--- templates/package-template/src/package-template.ts
+++ templates/package-template/src/package-template.ts
@@ -1,11 +1,12 @@
 //─────────────────────────────────────────────────────────────────────────────────────────────────┐
 /**
- * @file          packages/package-template/src/package-template.ts
- * @copyright     Vivek M. Chawla / Salesforce - 2019
  * @author        Vivek M. Chawla <@VivekMChawla>
+ * @copyright     2019, Vivek M. Chawla / Salesforce. All rights reserved.
+ * @license       BSD-3-Clause For full license text, see the LICENSE file in the repo root or
+ *                `https://opensource.org/licenses/BSD-3-Clause`
+ * @file          packages/package-template/src/package-template.ts
  * @summary       INSERT_SUMMARY_HERE
  * @description   INSERT_DESCRIPTION_HERE
- * @license       MIT
  */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┘
 // Import External Libraries, Modules, and Types
